10a86d6094f54448073fc5e718121884c2eb5f11
Michal Wittmayer <michal.wittmayer.025@pslib.cz>
2026-08-19 13:31:07 +0200

    Add FBS test data tools

---
+++ Experinmets/fake_data_maker.py
@@ -0,0 +1,25 @@
+import json
+import random
+from pathlib import Path
+
+filename = Path("~/Projects/FBS/Data/TestData/fake_data_more.json").expanduser()
+
+secs_in_day = 24 * 60 * 60
+
+def generate_fake_data():
+    data = []
+    for time in range(secs_in_day * 100):
+        entry = {
+            "lat": random.uniform(-40, 40),
+            "lon": random.uniform(-90, 90),
+            "time": time / 100 # 100 flashes per second
+        }
+        data.append(entry)
+    return data
+
+data = generate_fake_data()
+
+filename.parent.mkdir(parents=True, exist_ok=True)
+
+with open(filename, "w") as f:
+    json.dump(data, f)
